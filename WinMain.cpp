2e5a568c87e93738895a5c76152d421b1fa30d11
Robert Keller <rkiv@umich.edu>
2020-05-12 18:25:29 -0400

    Add basic CPP window class to wrap around the WinAPI Window class

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,7 +1,12 @@
-#include <Windows.h>
+#include "IvyWin.h"
+#include "Window.h"
+#include "WindowsMessageMap.h"
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+
 
 	switch (msg)
 	{
@@ -19,32 +24,8 @@ int CALLBACK WinMain(
 	int nCmdShow
 )
 {
-	const auto pClassName = "hw3d";
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
 
-	// create window instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, "Window Name");
 
 	// message pump
 	MSG msg;
@@ -55,13 +36,7 @@ int CALLBACK WinMain(
 		DispatchMessage(&msg);
 
 	}
-	if(gResult == -1)
-	{
-		return -1;
-	}
-	else
-	{
-		return msg.wParam;
-	}
-	return 0;
+
+	if(gResult == -1) return -1;
+	return msg.wParam;
 }
