27226c9c782c8d7f3de405f402c1022112375792
Robert Keller <rkiv@umich.edu>
2020-05-12 15:37:23 -0400

    Setup standard window procedure

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(13);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -12,7 +24,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -36,13 +48,20 @@ int CALLBACK WinMain(
 
 	// message pump
 	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	BOOL gResult;
+	while((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 
 	}
-	while (true);
-
+	if(gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 	return 0;
 }
