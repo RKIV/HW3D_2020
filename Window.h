56d706c36d631d18293dcd9dccebf08e77004ca2
Robert Keller <rkiv@umich.edu>
2020-05-13 19:21:17 -0400

    Add basic mouse input

--- Window.h
+++ Window.h
@@ -2,6 +2,7 @@
 #include "IvyWin.h"
 #include "IvyException.h"
 #include "Keyboard.h"
+#include "Mouse.h"
 
 class Window
 {
@@ -47,7 +48,8 @@ private:
 	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 public:
-	Keybaord kbd;
+	Keyboard kbd;
+	Mouse mouse;
 private:
 	int width;
 	int height;
