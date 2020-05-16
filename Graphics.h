d7daf5842733d43238aa80680f4b36a04ed36659
Robert Keller <rkiv@umich.edu>
2020-05-16 13:29:51 -0400

    More advanced DX debugging tools

--- Graphics.h
+++ Graphics.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IvyWin.h"
 #include "IvyException.h"
+#include "DxgiInfoManager.h"
 #include <d3d11.h>
 
 class Graphics
@@ -13,19 +14,23 @@ public:
 	class HrException : public Exception
 	{
 	public:
-		HrException(int line, const char* file, HRESULT hr) noexcept;
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
 		const char* what() const noexcept override;
 		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
 		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
 	private:
 		HRESULT hr;
+		std::string info;
 	};
 	class DeviceRemovedException : public HrException
 	{
 		using HrException::HrException;
 	public: 
 		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
 	};
 public:
 	Graphics(HWND hWnd);
@@ -35,6 +40,9 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
