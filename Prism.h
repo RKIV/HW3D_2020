f521243d674a7943da40d6b55a8c9822a77728ab
Robert Keller <rkiv@umich.edu>
2020-06-12 00:07:42 -0400

    Add cylinder object with independent faces

--- Prism.h
+++ Prism.h
@@ -81,6 +81,116 @@ public:
 		return { std::move(vertices), std::move(indices) };
 	}
 	template<class V>
+	static IndexedTriangleList<V> MakeTesselatedIndependentCapNormals(int longDiv)
+	{
+		namespace dx = DirectX;
+		assert(longDiv >= 3);
+
+		const dx::XMVECTOR base = dx::XMVectorSet(1.0f, 0.0f, -1.0f, 0.0f);
+		const dx::XMVECTOR offset = dx::XMVectorSet(0.0f, 0.0f, 2.0f, 0.0f);
+		const float longitudeAngle = 2.0f * PI / longDiv;
+
+		std::vector<V> vertices;
+
+		// near center
+		const unsigned short iCenterNear = (unsigned short)vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = { 0.0f, 0.0f, -1.0f, };
+		vertices.back().n = { 0.0f, 0.0f, -1.0f };
+		// near base vertices
+		const auto iBaseNear = (unsigned short)vertices.size();
+		for(int iLong = 0; iLong < longDiv; iLong++)
+		{
+			vertices.emplace_back();
+			dx::XMVECTOR v = dx::XMVector3Transform(
+				base,
+				dx::XMMatrixRotationZ(longitudeAngle * iLong)
+			);
+			dx::XMStoreFloat3(&vertices.back().pos, v);
+			vertices.back().n = { 0.0f, 0.0f, -1.0f };
+		}
+		// far center
+		const unsigned short iCenterFar = (unsigned short)vertices.size();
+		vertices.emplace_back();
+		vertices.back().pos = { 0.0f, 0.0f, 1.0f };
+		vertices.back().n = { 0.0f, 0.0f, 1.0f };
+		// far base vertices
+		const unsigned short iBaseFar = (unsigned short)vertices.size();
+		for (int iLong = 0; iLong < longDiv; iLong++)
+		{
+			vertices.emplace_back();
+			dx::XMVECTOR v = dx::XMVector3Transform(
+				base,
+				dx::XMMatrixRotationZ(longitudeAngle * iLong)
+			);
+			v = dx::XMVectorAdd(v, offset);
+			dx::XMStoreFloat3(&vertices.back().pos, v);
+			vertices.back().n = { 0.0f, 0.0f, 1.0f };
+		}
+		// fusilage vertices
+		const unsigned short iFusilage = (unsigned short)vertices.size();
+		for (int iLong = 0; iLong < longDiv; iLong++)
+		{
+			// near base
+			{
+				vertices.emplace_back();
+				dx::XMVECTOR v = dx::XMVector3Transform(
+					base,
+					dx::XMMatrixRotationZ(longitudeAngle * iLong)
+				);
+				dx::XMStoreFloat3(&vertices.back().pos, v);
+				vertices.back().n = { vertices.back().pos.x, vertices.back().pos.y, 0.0f };
+			}
+			// far base
+			{
+				vertices.emplace_back();
+				dx::XMVECTOR v = dx::XMVector3Transform(
+					base,
+					dx::XMMatrixRotationZ(longitudeAngle * iLong)
+				);
+				v = dx::XMVectorAdd(v, offset);
+				dx::XMStoreFloat3(&vertices.back().pos, v);
+				vertices.back().n = { vertices.back().pos.x, vertices.back().pos.y, 0.0f };
+			}
+		}
+		std::vector<unsigned short> indices;
+
+		// near base indices
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			const unsigned short i = iLong;
+			const unsigned short mod = longDiv;
+			// near
+			indices.push_back(i + iBaseNear);
+			indices.push_back(iCenterNear);
+			indices.push_back((i + 1) % mod + iBaseNear);
+		}
+		// far base indicies
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			const unsigned short i = iLong;
+			const unsigned short mod = longDiv;
+			// far
+			indices.push_back(iCenterFar);
+			indices.push_back(i + iBaseFar);
+			indices.push_back((i + 1) % mod + iBaseFar);
+		}
+		// fusilage indices
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			const unsigned short i = iLong * 2;
+			const auto mod = longDiv * 2;
+			indices.push_back(i + iFusilage);
+			indices.push_back((i + 2) % mod + iFusilage);
+			indices.push_back(i + 1 + iFusilage);
+			indices.push_back((i + 2) % mod + iFusilage);
+			indices.push_back((i + 3) % mod + iFusilage);
+			indices.push_back(i + 1 + iFusilage);
+		}
+
+		return { std::move(vertices), std::move(indices) };
+	}
+	template<class V>
 	static IndexedTriangleList<V> Make()
 	{
 		return MakeTesselated<V>(24);
